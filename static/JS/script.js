946282db1030a43a6eaa97a69cfee2705c67c6f2
Chukwuma Ebube <chukwuma619@gmail.com>
2023-09-09 21:57:22 +0100

    change styles to tailwindcsss

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Side Navigation
-  const open_nav_button = document.getElementById("open-nav");
-  const close_nav_button = document.getElementById("close-nav");
-  const side_nav = document.getElementById("aside");
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Side Navigation
+//   const open_nav_button = document.getElementById("open-nav");
+//   const close_nav_button = document.getElementById("close-nav");
+//   const side_nav = document.getElementById("aside");
 
-  open_nav_button.addEventListener("click", function () {
-    side_nav.classList.add("default-translate");
-  });
+//   open_nav_button.addEventListener("click", function () {
+//     side_nav.classList.add("default-translate");
+//   });
 
-  close_nav_button.addEventListener("click", function () {
-    side_nav.classList.remove("default-translate");
-  });
+//   close_nav_button.addEventListener("click", function () {
+//     side_nav.classList.remove("default-translate");
+//   });
 
-});
+// });
