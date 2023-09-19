69586cfc6afc0706544a56603dd1d9c986955d83
Chukwuma Ebube <chukwuma619@gmail.com>
2023-09-19 21:27:01 +0100

    finish all election pages

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,30 +1,28 @@
 // Navigation
 
-const buttonOpen = document.querySelector('#open-side-btn');
-const buttonClose = document.querySelector('#close-side-btn')
-const aside = document.querySelector('aside')
+const buttonOpen = document.querySelector("#open-side-btn");
+const buttonClose = document.querySelector("#close-side-btn");
+const aside = document.querySelector("aside");
 
 function sideNavBarToggle() {
-    // Function to toggle Side Navbar
-    aside.classList.toggle("translate-x-0")
+  // Function to toggle Side Navbar
+  aside.classList.toggle("translate-x-0");
 }
 
 // add event to Open/Close Nav Button
-buttonOpen.addEventListener('click', sideNavBarToggle)
-buttonClose.addEventListener('click', sideNavBarToggle)
+buttonOpen.addEventListener("click", sideNavBarToggle);
+buttonClose.addEventListener("click", sideNavBarToggle);
 
+// Election Dropdown Info on all candidate page
 
-// Election Deatils Button
+const electionDropdownInfo = document.querySelectorAll(".election-drop-info");
+electionDropdownInfo.forEach((dropButton) => {
+  dropButton.onclick = () => {
+    dropButton.querySelector("div").classList.toggle("hidden");
+  };
 
-const electionDetailsButton = document.querySelectorAll('#election-detail-btn');
-
-for (const button of electionDetailsButton) {
-    button.addEventListener('click', (e)=>{
-        const detailList = e.currentTarget.parentElement.querySelector('div')
-        detailList.style.display = "absolute"
-        console.log(detailList.classList);
-
-        // const  = parentButtonContainer.querySelector('div')
-        // console.log(detailList.classList);
-    })
-}
+  dropButton.querySelector("div").onmouseleave = (e) => {
+    dropButton.querySelector("div").classList.add("hidden");
+  }
+  
+});
